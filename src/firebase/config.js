8bdc64e860e150ac02cb699473e5a8b0cb210ecd
Thiago Marcel <thiagomarcel@Thiagos-MBP.fibertel.com.ar>
2024-11-20 22:36:27 -0300

    BoilerPlate - FireBase Config

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import app from 'firebase/app';
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyBk5D8CUQu_-bNxaz1DiwkvwhEYaKWMDJ0",
+    authDomain: "grupo6-reactnative.firebaseapp.com",
+    projectId: "grupo6-reactnative",
+    storageBucket: "grupo6-reactnative.firebasestorage.app",
+    messagingSenderId: "986250223306",
+    appId: "1:986250223306:web:e5cc4b3aa528c460dc5e33"
+};
+  app.initializeApp(firebaseConfig);
+
+  export const auth = firebase.auth();
+  export const storage = app.storage();
+  export const db = app.firestore();
+  
