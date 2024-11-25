1bd4211fa049b848a3adef220869e787337fdc25
TomasGarcia16 <tgarciamenese@udesa.edu.ar>
2024-11-25 08:46:50 -0300

    Home , sin los posts

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,46 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native-web';
+import { auth, db } from '../firebase/config';
+//falta importar post
 
-export default function Home(){
-  return(
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount(){
+    db.collection("posts").onSnapshot(
+      docs => {
+        let posts = []
+        docs.forEach((doc) => {
+          posts.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+        this.setState({
+          posts: posts
+        })
+      }
+    )
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Bienvenido</Text>
+        <FlatList
+        data={this.state.posts}
+        keyExtractor={(item) => item.id}
+        // renderItem={({item}) => (
+        //   <Post item = {item} />
+        // )}
+         />
+      </View>
+    );
+  }
 }
